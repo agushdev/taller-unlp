80a8329e3ca53ea6364b72e1af677a32c501372e
agushdev <agusponce055@gmail.com>
2025-10-02 18:56:30 -0300

    Objetos - practica 2 ejercicio 3 atroden

--- Objetos/Practica 2/ej3.java
+++ Objetos/Practica 2/ej3.java
@@ -19,15 +19,49 @@ public class ej3 {
             40 cupos de casting.
         */
         
-        int cupos=dias*personas, cuposOcupados=0, edadPersona, dniPersona;
+        int cupos=dias*personas, cuposOcupados=0, edadPersona, dniPersona, i=0, j=0;
         String nombrePersona;
         
+        System.out.println("Ingrese un nombre ('ZZZ' para finalizar): ");
         nombrePersona= GeneradorAleatorio.generarString(20);
-        while(!nombrePersona.equals("ZZZ") && cuposOcupados < 40){
-            dniPersona= GeneradorAleatorio.generarInt(99999);
-            edadPersona= GeneradorAleatorio.generarInt(70);
-            
+        
+        while(i < dias && !nombrePersona.equals("ZZZ") && cuposOcupados < 40){
+            j=0;
+            while(j < personas && !nombrePersona.equals("ZZZ") && cuposOcupados < 40){
+                System.out.println("Ingrese un dni: ");
+                dniPersona= GeneradorAleatorio.generarInt(99999);
+                System.out.println("Ingrese una edad: ");
+                edadPersona= GeneradorAleatorio.generarInt(70);
+                
+                matriz[i][j]= new Persona(nombrePersona,dniPersona,edadPersona);
+                cuposOcupados++;
+                
+                if (cuposOcupados < cupos){
+                    System.out.println("Ingrese un nombre ('ZZZ' para finalizar): ");
+                    nombrePersona= GeneradorAleatorio.generarString(20);
+                }
+                j++;
+            }
+            i++;
         }
         
+        /*
+            b) Informar para cada día y turno asignado, el nombre de la persona a entrevistar.
+            NOTA: utilizar la clase Persona. Pensar en la estructura de datos a utilizar. Para comparar
+            Strings use el método equals.
+        */
+        int contador=0;
+        
+        for (i=0; i<dias; i++){
+            for (j=0; j<personas; j++){
+                if (contador < cuposOcupados){
+                    System.out.println("Día " + (i + 1) + ", Turno " + (j + 1) + ": "
+                            + matriz[i][j].getNombre() + " - DNI "
+                            + matriz[i][j].getDNI() + " - Edad "
+                            + matriz[i][j].getEdad());
+                    contador++;
+                }
+            }
+        }
     }
 }
